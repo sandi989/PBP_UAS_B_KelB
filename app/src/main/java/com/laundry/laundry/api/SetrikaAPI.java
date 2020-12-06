418af3af6d7f2fe75a6df0c17ec859077e692ce3
sandi989 <sandi898hendrawan@gmail.com>
2020-12-07 01:35:18 +0700

    Final TUBES (Bonus PDF)

--- app/src/main/java/com/laundry/laundry/api/SetrikaAPI.java
+++ app/src/main/java/com/laundry/laundry/api/SetrikaAPI.java
@@ -1,7 +1,7 @@
 package com.laundry.laundry.api;
 
 public class SetrikaAPI {
-    public static final String ROOT_URL   = "http://cupanglaundry.000webhostapp.com/";
+    public static final String ROOT_URL   = "https://cupanglaundry.000webhostapp.com/";
     public static final String ROOT_API   = ROOT_URL+ "api/";
     public static final String URL_ADD    = ROOT_API+"setrika";
     public static final String URL_SELECT = ROOT_API+"setrika/";
